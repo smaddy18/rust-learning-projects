17760b51b10af3ddd7bf532dc2aa94a88c9f00d5
Konan KOUAME <konan.kouame@imt-atlantique.net>
2026-02-28 17:55:16 +0100

    refactoring with struct

--- chapiter5/rectangles/src/main.rs
+++ chapiter5/rectangles/src/main.rs
@@ -1,10 +1,20 @@
-fn main() {
-    let width = 30;
-    let height = 50;
 
-    println!("The area of the rectangle is {} square pixels.", area(width, height));
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    return width * height;
+fn main() {
+    let rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", area(&rectangle));
+    println!("rect is : {:#?}", rectangle);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    return rectangle.width * rectangle.height;
 }
