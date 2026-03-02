7718fe7603e85a7416193852f32b37b97f5091a3
Konan KOUAME <konan.kouame@imt-atlantique.net>
2026-03-02 02:44:28 +0100

    implemented methods for Rectangle struct

--- chapiter5/rectangles/src/main.rs
+++ chapiter5/rectangles/src/main.rs
@@ -5,16 +5,30 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        return self.width > other.width && self.height > other.height;
+    }
+}
+
 fn main() {
-    let rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rectangle));
-    println!("rect is : {:#?}", rectangle);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    return rectangle.width * rectangle.height;
+    println!("Can rect1 hold rect2 ? {},", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3 ? {},", rect1.can_hold(&rect3));
 }
